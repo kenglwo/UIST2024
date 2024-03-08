b65244dced066dcc1454dab53ab133f78f8d9891
Kento Shigyo <kentoglwo@gmail.com>
2024-03-08 12:07:54 +0800

    :sparkles: add text field and submit button

--- frontend/app/app/login/page.tsx
+++ frontend/app/app/login/page.tsx
@@ -2,6 +2,9 @@
 import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import Header from "../components/Header";
 import { UserInfo } from "../types";
 
@@ -23,9 +26,25 @@ export default function BasicGrid() {
         <Grid item xs={12}>
           <Header />
         </Grid>
-        <Grid item xs={6}>
-          <div></div>
+        <Grid item xs={4} />
+        <Grid item xs={4}>
+          <Stack>
+            <h1>Login Page</h1>
+            <TextField
+              id="user_id"
+              label="User ID"
+              variant="outlined"
+              sx={{ maxWidth: "200px" }}
+            />
+            <Button
+              variant="contained"
+              sx={{ maxWidth: "100px", marginTop: "20px" }}
+            >
+              Submit
+            </Button>
+          </Stack>
         </Grid>
+        <Grid item xs={4} />
       </Grid>
     </Box>
   );
